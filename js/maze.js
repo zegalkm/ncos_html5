e9fda62cd9907a97a72c0871c9689a0f411826de
Jessica <zegalkm@lycos.co.kr>
2012-09-21 18:00:48 +0900

    캐릭터 기본 이동 구현

--- js/maze.js
+++ js/maze.js
@@ -35,73 +35,62 @@ $(function(){
 			}
 	});
 
+	//캐릭터 이동범위
 	var movingDistance = 3;
+	//캐릭터 클릭 이벤트
 	$("#player").click(function(e){
-		var position = $("#player").position();
-		var x = position.left/movePiont;
+		var position = $("#player").position();//현재 캐릭터의 포지션을 구함
+		var x = position.left/movePiont;//무브포인트로 나누면 그것은 td의 id값!!
 		var y = position.top/movePiont;
 		
-		var x_e = x+movingDistance;
-		//우측
-		for(var i=x+1; i<=x_e;i++){
+		//우측 right 범위
+		var x_right = x+movingDistance;
+		for(var i=x+1; i<=x_right;i++){
 			$("td[id="+i+"_"+y+"]").addClass("select_td");
-			/**$("td[id="+i+"_"+y+"]").click(function(e){
-				var kk = (i-1)*movePiont;
-				$("#player").css('left',kk);
-				$("td").css("background-color","");
-			});*/
-			$("td[id="+i+"_"+y+"]").click(function(e){
-				var id = this.id;
-				if($("td[id="+id+"]").attr("class")=='select_td'){//왜 이럴까..
-					var k = id.split("_");
-					var kk = parseInt(k[0])*movePiont;
-					$("#player").animate({"left":kk+"px"},"slow");
-					$("td").removeClass("select_td");
-				}
-			});
+
 		}
-		var y_e = y+movingDistance;
-		//하단
-		for(var i=y+1; i<=y_e;i++){
-			$("td[id="+x+"_"+i+"]").addClass("select_td");
-			/**$("td[id="+i+"_"+y+"]").click(function(e){
-				var kk = (i-1)*movePiont;
-				$("#player").css('left',kk);
-				$("td").css("background-color","");
-			});*/
-			$("td[id="+x+"_"+i+"]").click(function(e){
-				var id = this.id;
-				if($("td[id="+id+"]").attr("class")=='select_td'){//왜 이럴까..
-					var k = id.split("_");
-					var kk = parseInt(k[1])*movePiont;
-					alert(kk);
-					$("#player").animate({"top":kk+"px"},"slow");
-					$("td").removeClass("select_td");
-				}
-			});
+		//좌측 left 범위
+		var x_left = x-movingDistance;
+		for(var i=x-1; i>=x_left;i--){
+			$("td[id="+i+"_"+y+"]").addClass("select_td");
 		}
-		y_e = y-movingDistance;
-		alert(y_e);
-		//상단
-		for(var i=y-1; i>=y_e;i--){
+
+		//하단 bottom 범위
+		var y_bottom = y+movingDistance;
+		for(var i=y+1; i<=y_bottom;i++){
 			$("td[id="+x+"_"+i+"]").addClass("select_td");
-			/**$("td[id="+i+"_"+y+"]").click(function(e){
-				var kk = (i-1)*movePiont;
-				$("#player").css('left',kk);
-				$("td").css("background-color","");
-			});*/
-			$("td[id="+x+"_"+i+"]").click(function(e){
-				alert(x+":"+i);
-				var id = this.id;
-				if($("td[id="+id+"]").attr("class")=='select_td'){//왜 이럴까..
-					var k = id.split("_");
-					var kk = parseInt(k[1])*movePiont;
-					alert(kk);
-					$("#player").animate({"top":kk+"px"},"slow");
-					$("td").removeClass("select_td");
-				}
-			});
+		}
+
+		//상단 top 범위
+		var y_top = y-movingDistance;
+		for(var i=y-1; i>=y_top;i--){
+			$("td[id="+x+"_"+i+"]").addClass("select_td");
 		}		
+	});
+
+	//캐릭터 이동 이벤트
+	$("td").click(function(){
+		var id=this.id;//선택학 td의 id값을 구한다.
+		var cssClz = $("#"+id).attr("class");//클래스값을 세팅한다.
+		if(cssClz!=undefined&&cssClz==='select_td'){ //이동가능 범위인 동시에 클래스가 있다면 실행
+			var xy = id.split("_"); 
+			var xxx = parseInt(xy[0])*movePiont;//스플릿한 id값. x값에 무빙포인트를 곱하면 x축으로 이동해야 할 거리가 나온다
+			var yyy = parseInt(xy[1])*movePiont;
+
+			var position = $("#player").position();
+			var px = position.left/movePiont;
+			var py = position.top/movePiont;
+
+			if(px!=xy[0]){//x축의 현재 캐릭터 위치와 이동해야 할 값이 다르면 실행
+				$("#player").animate({"left":xxx+"px"},500);
+			}
+			if(py!=xy[1]){
+				$("#player").animate({"top":yyy+"px"},500);
+			}
+			$("td").removeClass("select_td");
+		}else{
+			//아무것도 실행하지 않는다.
+		}
 
 	});
 
